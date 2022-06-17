687bc579c81e5cd68581f6fb0fc231481d13c2da
Seyoung Hong <maiorem00@gmail.com>
2022-06-17 22:05:36 +0900

    splash screen project

---
+++ splash_screen/splash_screen/android/app/src/main/kotlin/com/maiorem/splash_screen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.maiorem.splash_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ splash_screen/splash_screen/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'Hello World'
+          ),
+        ),
+      ),
+    )
+  );
+}
