17f0aab01085eab0c379a76777e7fe51e1394901
Seyoung Hong <maiorem00@gmail.com>
2022-06-18 10:16:06 +0900

    widget class

--- splash_screen/splash_screen/lib/main.dart
+++ splash_screen/splash_screen/lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello World'
-          ),
-        ),
+  runApp(MaterialApp(
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(
+        child: Text('Hello World2'),
       ),
-    )
-  );
+    );
+  }
 }
