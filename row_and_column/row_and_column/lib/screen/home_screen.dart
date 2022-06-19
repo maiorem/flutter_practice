c071dbd90dd0cae135fddf0d4d0aa09295242453
Seyoung Hong <maiorem00@gmail.com>
2022-06-20 00:17:12 +0900

    mainAxisAligment

--- row_and_column/row_and_column/lib/screen/home_screen.dart
+++ row_and_column/row_and_column/lib/screen/home_screen.dart
@@ -10,7 +10,15 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: Colors.black,
-          child: Column(
+          child: Row(
+            // MainAxisAlignment - 주축 정렬
+            // start - 시작
+            // end - 끝
+            // center - 가운데
+            // spaceBetwwen - 위젯과 위젯이 끝과 끝, 그 사이가 동일하게 배치
+            // spaceEvenly - 끝과 끝을 빈 간격으로 시작하고 그 사이에 위젯을 동일하게 배치
+            // spaceAround - space Evnenly + 끝과 끝의 간격은 1/2
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Container(
                 color: Colors.red,
