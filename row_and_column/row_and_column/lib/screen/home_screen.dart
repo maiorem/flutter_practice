bb33422a5d59e745e92a4778f3dfdca8fdedf624
Seyoung Hong <maiorem00@gmail.com>
2022-06-25 21:57:15 +0900

    expanded and flexible

--- row_and_column/row_and_column/lib/screen/home_screen.dart
+++ row_and_column/row_and_column/lib/screen/home_screen.dart
@@ -10,8 +10,8 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: Colors.black,
-          height: MediaQuery.of(context).size.width,
-          child: Row(
+          // height: MediaQuery.of(context).size.width,
+          child: Column(
             // MainAxisAlignment - 주축 정렬
             // start - 시작
             // end - 끝
@@ -21,23 +21,37 @@ class HomeScreen extends StatelessWidget {
             // spaceAround - space Evenly + 끝과 끝의 간격은 1/2
             mainAxisAlignment: MainAxisAlignment.start,
             // CrossAxisAlignment - 반대축 정렬
-            //
+            // stretch : 최대한으로 늘린다
             crossAxisAlignment: CrossAxisAlignment.start,
+            // MainAxisSize - 주축의 크기
+            mainAxisSize: MainAxisSize.max,
             children: [
-              Container(
-                color: Colors.red,
-                width: 50.0,
-                height: 50.0,
+              // Expanded : 최대한으로 남아있는 사이즈를 모두 차지하기
+              // Expanded끼리 나눠먹음
+              Expanded(
+                flex: 2, // 나머지 공간을 나눠먹는 비율
+                child: Container(
+                  color: Colors.red,
+                  width: 50.0,
+                  height: 50.0,
+                ),
               ),
-              Container(
-                color: Colors.orange,
-                width: 50.0,
-                height: 50.0,
+
+              Expanded(
+                child: Container(
+                  color: Colors.orange,
+                  width: 50.0,
+                  height: 50.0,
+                ),
               ),
-              Container(
-                color: Colors.yellow,
-                width: 50.0,
-                height: 50.0,
+              // Flexible : width와 height를 지키고 나머지를 주축 배경으로 채움(버림)
+              Flexible(
+                flex: 2, // 버리는 공간 비율
+                child: Container(
+                  color: Colors.yellow,
+                  width: 50.0,
+                  height: 50.0,
+                ),
               ),
               Container(
                 color: Colors.green,
