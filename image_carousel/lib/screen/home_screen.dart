8f89b47290d8e3f2ea9c0bf198a440ef09e20dd5
Seyoung Hong <maiorem00@gmail.com>
2022-07-01 22:57:52 +0900

    pageController animateToPage

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,17 +13,33 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
   Timer? timer;
+  PageController pageController = PageController(
+    initialPage: 0,
+  );
 
   @override
   void initState() {
     super.initState();
-    timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      print('Timer!');
+    timer = Timer.periodic(Duration(seconds: 4), (timer) {
+      int currentPage = pageController.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
+
+      pageController.animateToPage(
+          nextPage,
+          duration: Duration(milliseconds: 1000),
+          curve: Curves.linear,
+      );
     });
   }
 
+  // 적당한 시기에 꺼주기
   @override
   void dispose() {
+    pageController.dispose();
     if (timer != null) {
       timer!.cancel();
     }
@@ -31,8 +48,11 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return Scaffold(
       body: PageView(
+        controller: pageController,
         children: [1, 2, 3, 4, 5]
             .map(
               (e) => Image.asset(
