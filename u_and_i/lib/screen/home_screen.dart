0359bb89eeea40f8e6fe7f14bd4710fb9c7ed961
Seyoung Hong <maiorem00@gmail.com>
2022-07-10 20:31:34 +0900

    font style

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -10,6 +10,53 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.orange[100],
+      body: SafeArea(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Text(
+                'ILL&Walk',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'parisienne',
+                  fontSize: 50.0,
+                ),
+              ),
+              Text(
+                '내일 또 출근 하는 날',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 30.0),
+              ),
+              Text(
+                '2022. 07.10',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 20.0),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.favorite,
+                ),
+              ),
+              Text(
+                'D+1',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'sunflower',
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
