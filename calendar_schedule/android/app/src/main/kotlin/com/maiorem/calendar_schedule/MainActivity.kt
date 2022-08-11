9bec82d037c76935be84a9a4f3c295bcce4baa71
Seyoung Hong <maiorem00@gmail.com>
2022-08-11 20:46:54 +0900

    calendar app settings

---
+++ calendar_schedule/android/app/src/main/kotlin/com/maiorem/calendar_schedule/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.maiorem.calendar_schedule
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ calendar_schedule/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:calendar_schedule/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans'
+      ),
+      home: HomeScreen()
+    )
+  );
+}
