41bd86c559c00718b48af517837a77cac1bbf9b3
Seyoung Hong <maiorem00@gmail.com>
2022-08-15 14:31:45 +0900

    table calendar style

--- calendar_schedule/lib/component/calendar.dart
+++ calendar_schedule/lib/component/calendar.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+
+  DateTime? selectedDay;
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +26,19 @@ class Calendar extends StatelessWidget {
             fontSize: 16.0,
           )
         ),
+      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+        setState(() {
+          this.selectedDay = selectedDay;
+        });
+      },
+      selectedDayPredicate: (DateTime date){
+          if (selectedDay == null) {
+            return false;
+          }
+          return date.year == selectedDay!.year &&
+            date.month == selectedDay!.month &&
+            date.day == selectedDay!.day;
+      },
     );
   }
 }
